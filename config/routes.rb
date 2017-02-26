c70700cd8396d9f3992a84459e55fcfc43af47dc
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2017-02-26 13:16:05 +0000

    devise twitter auth under implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'test#index'
+	devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   get 'test/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
